139863dd603248173e711c8e0bcc141dec21e576
mzp <mzpppp@gmail.com>
2014-03-22 20:56:11 +0900

    specify cohttp version

--- opam.sh
+++ opam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-opam install -y ssl lwt cohttp meta_conv tiny_json tiny_json_conv batteries bitstring cryptokit
+opam install -y ssl lwt cohttp.0.9.16 meta_conv tiny_json tiny_json_conv batteries bitstring cryptokit
